eb8dc8e43049fb57ab7577634b20e3cc175e2d6d
Dwane Dunn <dwane.dunn@gmail.com>
2025-06-25 23:55:50 +0000

    update NavBar import in profile

--- src/front/pages/Profile.jsx
+++ src/front/pages/Profile.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react"
 import rigoImageUrl from "../assets/img/rigo-baby.jpg";
 import useGlobalReducer from "../hooks/useGlobalReducer.jsx";
-import { NavBar } from "../components/Navbar.jsx";
+import { Navbar } from "../components/Navbar.jsx";
 
 // This component is used to display the profile page of the user.
 // Coppied from the Home Component
@@ -10,35 +10,11 @@ export const Profile = () => {
 
 	const { store, dispatch } = useGlobalReducer()
 
-	// const loadMessage = async () => {
-	// 	try {
-	// 		const backendUrl = import.meta.env.VITE_BACKEND_URL
 
-	// 		if (!backendUrl) throw new Error("VITE_BACKEND_URL is not defined in .env file")
-
-	// 		const response = await fetch(backendUrl + "/api/hello")
-	// 		const data = await response.json()
-
-	// 		if (response.ok) dispatch({ type: "set_hello", payload: data.message })
-
-	// 		return data
-
-	// 	} catch (error) {
-	// 		if (error.message) throw new Error(
-	// 			`Could not fetch the message from the backend.
-	// 			Please check if the backend is running and the backend port is public.`
-	// 		);
-	// 	}
-
-	// }
-
-	// useEffect(() => {
-	// 	loadMessage()
-	// }, [])
 
 	return (
 		<>
-			<NavBar />
+			<Navbar />
 			<div className="text-center mt-5">
 				<h1 className="display-4">Profile Page</h1>
 				<form>
@@ -58,15 +34,7 @@ export const Profile = () => {
 						<button>Exit</button>
 					</div>
 				</form>
-				{/* <div className="alert alert-info">
-				{store.message ? (
-					<span>{store.message}</span>
-				) : (
-					<span className="text-danger">
-						Loading message from the backend (make sure your python 🐍 backend is running)...
-					</span>
-				)}
-			</div> */}
+
 			</div>
 		</>
 	);
