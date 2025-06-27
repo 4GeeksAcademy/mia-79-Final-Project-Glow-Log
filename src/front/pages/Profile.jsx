ef29c645e5f480c55892b098ddbe152ea4652548
Dwane Dunn <dwane.dunn@gmail.com>
2025-06-27 18:35:26 +0000

    refactor labels, inputs & buttons further with bootstrap

--- src/front/pages/Profile.jsx
+++ src/front/pages/Profile.jsx
@@ -18,19 +18,19 @@ export const Profile = () => {
 				<h1 className="display-4">Profile Page</h1>
 				<form>
 					<div class="mb-3">
-						<label for="FormControlName" class="form-label">Name</label>
-						<input type="text" class="form-control" id="FormControlName" placeholder="Enter a Name" />
-						<label for="exampleFormControlemail" class="form-label">Email address</label>
-						<input type="email" class="form-control" id="exampleFormControlemail" placeholder="name@example.com" />
+						<label for="name" class="form-label">Name</label>
+						<input type="text" class="form-control" id="name" placeholder="Enter a Name" />
+						<label for="email" class="form-label">Email address</label>
+						<input type="email" class="form-control" id="email" placeholder="name@example.com" />
 					</div>
 					<div class="mb-3">
-						<label for="FormControlPassword" class="form-label">Password</label>
-						<input class="form-control" id="FormControlPassword" rows="3" />
+						<label for="password" class="form-label">Password</label>
+						<input class="form-control" id="password" rows="3" />
 					</div>
 
 					<div className="button-wrap">
-						<button type="submit">Save</button>
-						<button>Exit</button>
+						<button className="btn btn-primary" type="submit">Save</button>
+						<button className="btn btn-outline-secondary" type="button">Exit</button>
 					</div>
 				</form>
 
