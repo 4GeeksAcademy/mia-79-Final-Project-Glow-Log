76ea3547369de529846aa0a5887ec3a586219f15
Dwane Dunn <dwane.dunn@gmail.com>
2025-07-09 22:42:37 +0000

    fix uploadwidget name

--- src/front/pages/AddProduct.jsx
+++ src/front/pages/AddProduct.jsx
@@ -375,7 +375,7 @@ export default function AddProduct() {
       </form>
       {/* Cloudinary Upload Widget  */}
       <div className="mt-4 text-center">
-        <CloudinaryUploadWidget uwConfig={uwConfig} setPublicId={setPublicId} />
+        <UploadWidget uwConfig={uwConfig} setPublicId={setPublicId} />
         {publicId && (
           <div className="mt-2">
             <AdvancedImage cldImg={cld.image(publicId)} />
