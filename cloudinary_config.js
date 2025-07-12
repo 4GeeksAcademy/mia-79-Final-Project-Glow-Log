70e15a9288e1a44c72300ede3827e3bb4caf0bb1
Dwane Dunn <dwane.dunn@gmail.com>
2025-07-12 16:43:49 +0000

    remove items from locations

--- cloudinary_config.js
+++ cloudinary_config.js
@@ -11,10 +11,6 @@ function showUploadWidget() {
         'camera',
         'image_search',
         'google_drive',
-        'dropbox',
-        'shutterstock',
-        'getty',
-        'istock',
         'unsplash',
       ],
       googleApiKey: '<image_search_google_api_key>',
@@ -54,3 +50,37 @@ function showUploadWidget() {
     }
   );
 }
+
+// backup of values
+// showAdvancedOptions: true,
+//       cropping: true,
+//       multiple: false,
+//       defaultSource: 'local',
+//       styles: {
+//         palette: {
+//           window: '#c5d09f',
+//           windowBorder: '#5d6c3b',
+//           tabIcon: '#5d6c3b',
+//           menuIcons: '#5d6c3b',
+//           textDark: '#dde6c4',
+//           textLight: '#FFFFFF',
+//           link: '#5d6c3b',
+//           action: '#5d6c3b',
+//           inactiveTabIcon: '#0E2F5A',
+//           error: '#9daf7a',
+//           inProgress: '#768f58',
+//           complete: '#768f58',
+//           sourceBg: '#dde6c4',
+//         },
+
+// sources: [
+//         'local',
+//         'url',
+//         'camera',
+//         'image_search',
+//         'google_drive',
+//         'dropbox',
+//         'shutterstock',
+//         'getty',
+//         'istock',
+//         'unsplash',
