3c9add5c6b350bea9bd58db982b3d40e60a05a4f
jessica-dallas <135188047+jessica-dallas@users.noreply.github.com>
2025-08-05 18:41:52 -0500

    Revert "fixed updated product button on editproduct page to navigate back to …"

--- src/front/pages/EditProduct.jsx
+++ src/front/pages/EditProduct.jsx
@@ -117,7 +117,6 @@ export default function EditProduct() {
                 setNewCategory("");
                 setAddingBrand(false);
                 setNewBrand("");
-                navigate("/"); // Navigate back to the product log or home page
             } else {
                 const errorData = await response.json();
                 alert(`Error: ${errorData.message}`);
