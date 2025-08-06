47e162eccaaaa4584ed788db270c49f3d5b19ff8
almost-annonymous <anam.dhala@gmail.com>
2025-08-06 00:34:01 +0000

    added navigate back to log page after adding and updating products

--- src/front/pages/AddProduct.jsx
+++ src/front/pages/AddProduct.jsx
@@ -152,6 +152,7 @@ export default function AddProduct() {
         setNewCategory("");
         setAddingBrand(false);
         setNewBrand("");
+        navigate("/"); // Navigate back to the product log or home page
       } else {
         const errorData = await response.json();
         alert(`Error: ${errorData.message}`);
